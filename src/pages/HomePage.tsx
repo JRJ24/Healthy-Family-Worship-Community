420f37425bc7991ab68b45e45f0f1dbdc8fee506
JRJ24 <jonaifryrodriguezdejesus5@gmail.com>
2025-11-25 12:56:11 -0400

    feat: I Dont Remember what change in this commit sorry

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,13 +6,14 @@ import AboutUs from './../components/HomePage/AboutUs.tsx';
 import MapLocation from './../components/HomePage/MapaLocation.tsx';
 import Footer from './../components/Footer.tsx';
 import cafsaMain from './../../public/CAFSA.svg';
+import cafsaBookClub from './../../public/BookClub.png';
 
 const images = [
   cafsaMain,
   "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  cafsaBookClub,
   "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 ];
 
